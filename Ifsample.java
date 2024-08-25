ea0f3a4252d90737d6ff444808b2be98841b67f0
Sebin Joseph <josephsebin012@gmail.com>
2024-08-25 22:13:57 +0530

    Add files via upload

---
+++ Ifsample.java
@@ -0,0 +1,19 @@
+package java1;
+
+import java.util.Scanner;
+
+public class Ifsample {
+	public static void main (String[]args) {
+		
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter a number");
+		
+		int num=s.nextInt();
+		
+		if(num<0) {
+			System.out.println("the number is negative");
+		}else
+		System.out.println("the number is positive");
+	}
+
+}
