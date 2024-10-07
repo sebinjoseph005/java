ef7e0e317056831c9e58560676969b1666b4b188
Sebin Joseph <josephsebin012@gmail.com>
2024-10-07 23:55:42 +0530

    Add files via upload

---
+++ If_statement2.java
@@ -0,0 +1,19 @@
+package java1;
+
+public class If_statement2 {
+	public static void main(String[]args) {
+		
+		int age=9;
+		
+		if(age>=18){
+			System.out.println("you are an adult....");
+		}
+		
+		else if(age>=13){
+			System.out.println("you are an teenager....");
+		}
+		else{
+			System.out.println("you are a kid....");
+		}
+	}
+}
