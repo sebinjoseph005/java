fecb7f8856390e01ac7b0e20735fc2e17b7fb990
Sebin Joseph <josephsebin012@gmail.com>
2024-10-07 23:54:30 +0530

    Add files via upload

---
+++ Math_class.java
@@ -0,0 +1,19 @@
+package java1;
+
+public class Math_class {
+	public static void main(String[]args) {
+		
+		
+	double x=3.14;
+	double y=-10;
+	
+	//double z=Math.max(y, x);
+	//double z=Math.min(y, x);
+	//double z=Math.abs(y); 
+	//double z=Math.round( x);
+	//double z=Math.ceil( x);
+	double z=Math.floor( x);
+	System.out.println(z);
+	
+	}
+}
