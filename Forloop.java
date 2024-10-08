4e393d747af63e575b89bf1f4a06f4f175d3fac5
Sebin Joseph <josephsebin012@gmail.com>
2024-10-08 23:48:29 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,16 @@
+package java1;
+
+public class Forloop {
+	public static void main (String[]args) {
+		//for loop=executes a block of code for a limited amount of times
+		
+		//for(int i=0;i<=10;i++)
+		//i-=2
+		for(int i=10;i>=0;i--){
+			System.out.println(i);
+		}
+		
+		System.out.println("happy new year.....");
+	}
+	
+}
